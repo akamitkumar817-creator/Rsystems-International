935cc8b5905791cb76ff8332b668b6deaa1fd56a
akamitkumar817-creator <akamitkumar817@gmail.com>
2026-07-24 21:57:57 +0530

    resume and rg added

--- amit-rg.tf
+++ amit-rg.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "central india"
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "example2-resources"
+  location = "central india"
+}
